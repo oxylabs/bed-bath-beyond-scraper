de998c50f8c1213a237a51cf66d1dda90dce0890
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-02-20 14:50:03 +0200

    Adding code examples/bed-bath-beyond_curl.sh

---
+++ code examples/bed-bath-beyond_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.bedbathandbeyond.com/c/furniture/living-room-furniture?t=24359"}'
